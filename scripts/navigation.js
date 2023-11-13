58fb289aa7ba79d0c73f60d1c2427af127dc9c81
Yosi Ben Shushan <Yosi.Ben.shushan1@s.afeka.ac.il>
2023-11-13 21:10:16 +0200

    Commiting before big change

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,21 +1,15 @@
 
 const sidebarBtnElem = document.querySelectorAll('.navigation-button');
-//console.log();
 
-console.log(sidebarBtnElem);
-
-
+highlightSectionButton(sidebarBtnElem);
 
 sidebarBtnElem.forEach(button => {
   button.addEventListener('click', () => {
     const buttonText = button.querySelector('.nav-btn-text').innerHTML;
     console.log(`the button ${buttonText} was pressed `);
     scrollToSection( buttonText.toLowerCase());
-    
   })
 })
-
-
 
 
 function scrollToSection(sectionId){
@@ -24,4 +18,22 @@ function scrollToSection(sectionId){
   if(section) {
     section.scrollIntoView({behavior: 'smooth'});
   }
+}
+
+
+function highlightSectionButton(sidebarBtnElem) {
+  window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    sidebarBtnElem.forEach(button => {
+      const targetId = button.getAttribute('data-target');
+      const targetSection = document.getElementById(targetId);
+      const sectionTop = targetSection.offsetTop;
+      const sectionBottom = sectionTop + targetSection.offsetHeight;
+      if(scrollPosition >= sectionTop - 20 && scrollPosition < sectionBottom) {
+        button.classList.add('highlighted');
+      }else{
+        button.classList.remove('highlighted');
+      }
+    });
+  });
 }
